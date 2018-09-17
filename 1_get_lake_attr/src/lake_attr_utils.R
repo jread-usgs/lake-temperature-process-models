03a592211923b4c2679623ebb8e5601559151b20
jzwart <jzwart@usgs.gov>
2018-09-17 11:51:14 -0500

    getting base nml from mda.lakes function

--- 1_get_lake_attr/src/lake_attr_utils.R
+++ 1_get_lake_attr/src/lake_attr_utils.R
@@ -1,2 +1,14 @@
 # functions for gathering lake attributes
 
+
+
+
+
+
+
+get_base_lake_nml <- function(nhd_id, nml_out) {
+  nml <- mda.lakes::populate_base_lake_nml(site_id = nhd_id,
+                                driver = file.path("2_setup_models/meteo",
+                                                   paste0(nhd_id, "_driver.csv")))
+  write_nml(glm_nml = nml, file = nml_out)
+}
