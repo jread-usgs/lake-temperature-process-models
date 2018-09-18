b9a74c258a2eea4cf4f248a7945180d1d4848dfd
jzwart <jzwart@usgs.gov>
2018-09-18 16:04:41 -0500

    updating nml makefile for driver data path on Yeti

--- 1_get_lake_attr/src/create_nml_tasks.R
+++ 1_get_lake_attr/src/create_nml_tasks.R
@@ -1,20 +1,27 @@
 
-create_nml_tasks <- function(lake_list){
+create_nml_tasks <- function(lake_list, feature_nldas_coords){
 
   tasks <- lake_list %>% as_tibble() %>%
+    left_join(feature_nldas_coords %>%
+                as_tibble() %>%
+                select(site_id, nldas_coord_x, nldas_coord_y) %>%
+                mutate(site_id = as.character(site_id)), # site_id is a factor for feature_nldas_coords
+              by = 'site_id') %>%
     dplyr::rename(tn = site_id)
 
   base_nml <- scipiper::create_task_step(
     step_name = 'base_nml',
     target_name = function(task_name, ...){
       cur_task <- dplyr::filter(tasks, tn==task_name)
-      sprintf('glm_%s.nml', task_name)
+      sprintf('1_get_lake_attr/out/glm_%s.nml', task_name)
     },
     command = function(task_name, ...){
       cur_task <- dplyr::filter(tasks, tn==task_name)
       psprintf(
         "get_base_lake_nml(",
         "nhd_id = I('%s')," = cur_task$tn,
+        "nldas_x = I('%s')," = cur_task$nldas_coord_x,
+        "nldas_y = I('%s')," = cur_task$nldas_coord_y,
         "nml_out = target_name)")
     })
 
--- 1_get_lake_attr/src/lake_attr_utils.R
+++ 1_get_lake_attr/src/lake_attr_utils.R
@@ -7,12 +7,10 @@ lake_list <- function(crosswalk_file){
   return(lake_list)
 }
 
-
-
-get_base_lake_nml <- function(nhd_id, nml_out) {
+get_base_lake_nml <- function(nhd_id, nldas_x, nldas_y, nml_out) {
   # change Driver file path # ****************************************
   nml <- mda.lakes::populate_base_lake_nml(site_id = nhd_id,
-                                driver = file.path("2_setup_models/meteo",
-                                                   paste0(nhd_id, "_driver.csv")))
+                                driver = file.path('driver-data', # file path on Yeti - this may change depending on where the nml files live (I think)
+                                                   paste0('NLDAS_time[0.346848]_x[',nldas_x,']_y[',nldas_y,'].csv')))
   write_nml(glm_nml = nml, file = nml_out)
 }
