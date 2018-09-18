36009c461c786e90982c392e20c1d4402ea1d072
jzwart <jzwart@usgs.gov>
2018-09-18 16:05:18 -0500

    adding comments

--- 1_get_lake_attr/src/lake_attr_utils.R
+++ 1_get_lake_attr/src/lake_attr_utils.R
@@ -11,6 +11,6 @@ get_base_lake_nml <- function(nhd_id, nldas_x, nldas_y, nml_out) {
   # change Driver file path # ****************************************
   nml <- mda.lakes::populate_base_lake_nml(site_id = nhd_id,
                                 driver = file.path('driver-data', # file path on Yeti - this may change depending on where the nml files live (I think)
-                                                   paste0('NLDAS_time[0.346848]_x[',nldas_x,']_y[',nldas_y,'].csv')))
+                                                   paste0('NLDAS_time[0.346848]_x[',nldas_x,']_y[',nldas_y,'].csv'))) #times are all the same, but could make this an input
   write_nml(glm_nml = nml, file = nml_out)
 }
