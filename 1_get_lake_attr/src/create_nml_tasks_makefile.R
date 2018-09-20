90c1a2acb296bb6d9c4a0bbd5f007913c0ed0029
jzwart <jzwart@usgs.gov>
2018-09-20 15:55:49 -0500

    adding package dependency

--- 1_get_lake_attr/src/create_nml_tasks_makefile.R
+++ 1_get_lake_attr/src/create_nml_tasks_makefile.R
@@ -5,7 +5,7 @@ create_nml_tasks_makefile <- function(makefile, task_plan, remake_file){
     makefile = makefile,
     task_plan = task_plan,
     include = remake_file,
-    packages = c('dplyr', 'scipiper', 'mda.lakes'),
+    packages = c('dplyr', 'scipiper', 'mda.lakes', 'lakeattributes'),
     sources = c('1_get_lake_attr/src/lake_attr_utils.R'),
     file_extensions = c('ind'))
 }
