25109688c49847f838cacef43450fb6115c1d69b
Jordan S Read <jread@usgs.gov>
2019-10-30 15:21:24 -0500

    missing arg_name

--- 3_run/src/run_glm_utils.R
+++ 3_run/src/run_glm_utils.R
@@ -4,8 +4,8 @@ run_glm <- function(sim_dir, nml_obj, export_file = NULL){
 
   glmtools::write_nml(nml_obj, file.path(sim_dir, 'glm2.nml'))
   GLMr::run_glm(sim_dir, verbose = FALSE)
-  out_dir <- glmtools::get_nml_value('out_dir')
-  out_file <- paste0(glmtools::get_nml_value('out_fn'), '.nc')
+  out_dir <- glmtools::get_nml_value(nml_obj, arg_name = 'out_dir')
+  out_file <- paste0(glmtools::get_nml_value(nml_obj, arg_name = 'out_fn'), '.nc')
   nc_path <- file.path(sim_dir, out_dir, out_file)
 
   if (!is.null(export_file)){
