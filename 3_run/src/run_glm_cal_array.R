b04c7fc976f6c454d04de09b59fbf191df268654
Jordan S Read <jread@usgs.gov>
2020-01-16 23:20:00 -0600

    issue w/ nml filepath

--- 3_run/src/run_glm_cal_array.R
+++ 3_run/src/run_glm_cal_array.R
@@ -21,6 +21,7 @@ nml_obj <- glmtools::read_nml(this_job$nml_file)
 base_meteo <- this_job$meteo_file
 # write the file to here:
 meteo_filepath <- file.path(sim_dir, paste0(this_job$sim_id, '.csv'))
+nml_filepath <- file.path(sim_dir, basename(this_job$nml_file))
 nml_obj <- glmtools::set_nml(nml_obj, arg_name = 'meteo_fl', basename(meteo_filepath))
 meteo_data <- readr::read_csv(base_meteo)
 readr::write_csv(driver_add_rain(meteo_data), path = meteo_filepath)
@@ -30,8 +31,8 @@ readr::write_csv(driver_add_rain(meteo_data), path = meteo_filepath)
 feather::read_feather('2_prep/out/temperature_obs.feather') %>% filter(site_id == this_job$site_id) %>%
   select(DateTime = date, Depth = depth, temp) %>% readr::write_csv(caldata_fl)
 
-glmtools::write_nml(glm_nml = nml_obj, file = this_job$nml_file)
-nml_cal <- run_glm_cal(nml_file = this_job$nml_file, sim_dir, caldata_fl = caldata_fl)
+glmtools::write_nml(glm_nml = nml_obj, file = nml_filepath)
+nml_cal <- run_glm_cal(nml_file = nml_filepath, sim_dir, caldata_fl = caldata_fl)
 glmtools::write_nml(glm_nml = nml_cal, this_job$export_file)
 
 unlink(sim_dir, recursive = TRUE)
