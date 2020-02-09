6480b81a495773a99cdc775c4e4a812c46405d5d
Jordan S Read <jread@usgs.gov>
2020-02-09 14:17:14 -0600

    is this screwing up Yeti Scratch?

--- 3_run/src/run_glm_utils.R
+++ 3_run/src/run_glm_utils.R
@@ -6,7 +6,11 @@ run_glm <- function(sim_dir, nml_obj, export_file = NULL){
   GLMr::run_glm(sim_dir, verbose = FALSE)
   out_dir <- glmtools::get_nml_value(nml_obj, arg_name = 'out_dir')
   out_file <- paste0(glmtools::get_nml_value(nml_obj, arg_name = 'out_fn'), '.nc')
-  nc_path <- file.path(sim_dir, out_dir, out_file)
+  if (out_dir == '.'){
+    nc_path <- file.path(sim_dir, out_file)
+  } else {
+    nc_path <- file.path(sim_dir, out_dir, out_file)
+  }
 
   if (!is.null(export_file)){
     export_temp(filepath = export_file, nml_obj, nc_filepath = nc_path)
