39a96821c7fa5702684f7cf3b37b6f86763cd45a
Jordan S Read <jread@usgs.gov>
2020-04-08 10:37:56 -0500

    update to TOHA filtering

--- 1_fetch/src/fetch_utils.R
+++ 1_fetch/src/fetch_utils.R
@@ -7,7 +7,12 @@ filter_nml_list <- function(nml_list_unfiltered_rds) {
 }
 
 
-filter_toha_models <- function(model_ids, kw_data){
+filter_toha_models <- function(nml_list, kw_data){
 
-  model_ids[model_ids %in% unique(kw_data$site_id)]
+  nml_with_kw_file <- nml_list[names(nml_list) %in% unique(kw_data$site_id)]
+
+  model_hypso_ids <- sapply(names(nml_with_kw_file), function(x){
+    length(nml_with_kw_file[[x]]$H) > 2
+  })
+  names(model_hypso_ids)[model_hypso_ids]
 }
