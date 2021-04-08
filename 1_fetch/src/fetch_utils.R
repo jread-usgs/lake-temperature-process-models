1aa27c3d388419c6a0981be75c36917a03686422
Jordan S Read <jread@usgs.gov>
2021-04-08 11:58:54 -0500

    hack to deal w/ new met driver data

--- 1_fetch/src/fetch_utils.R
+++ 1_fetch/src/fetch_utils.R
@@ -1,8 +1,11 @@
 #' Filter the nml args list to only those sites that have a meteo file available
 filter_nml_list <- function(nml_list_unfiltered_rds) {
   nml_list_unfiltered <- readRDS(nml_list_unfiltered_rds)
-  meteo_fl <- sapply(nml_list_unfiltered, `[[`, 'meteo_fl')
-  nml_list <- nml_list_unfiltered[file.exists(file.path('../lake-temperature-model-prep/7_drivers_munge/out', meteo_fl))]
+  meteo_fl <- sapply(nml_list_unfiltered, `[[`, 'meteo_fl') %>%
+    gsub(pattern = '0.367700', replacement = '0.359420', x = .)
+  # need to replace NLDAS_time[0.367700] with NLDAS_time[0.359420]
+
+  nml_list <- nml_list_unfiltered[file.exists(file.path('/Volumes/ThunderBlade/NLDAS_GLM', meteo_fl))]
   return(nml_list)
 }
 
